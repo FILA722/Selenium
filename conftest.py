dd944453d9c2978c0ec6c23f9285a00c01ba421a
Alex Fila <filal41sng@gmail.com>
2021-06-18 12:32:31 +0300

    conftest.py & test_main_page.py ready to work

--- conftest.py
+++ conftest.py
@@ -3,13 +3,12 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_language', action='store', default='ru')
+    parser.addoption('--language', action='store', default='en')
 
 @pytest.fixture(scope='function')
 def browser(request):
-    browser_language = request.config.getoption('browser_language')
-    browser = webdriver.Chrome()
-    link = f'http://selenium1py.pythonanywhere.com/{browser_language}/catalogue/coders-at-work_207/'
-    browser.get(link)
+    language = request.config.getoption('language')
+    browser = webdriver.Firefox()
+    # link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/'
+    # browser.get(link)
     return browser
-    # browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,9 @@
+# def test_button(browser):
+#     try:
+#         assert  browser.find_elements_by_css_selector('button.btn:nth-child(3)'), 'Кнопка не найдена'
+#     finally:
+#         browser.quit()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
