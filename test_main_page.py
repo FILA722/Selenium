bb320fb4e47d21ec26c8c478ed63f61bb5374e64
Alex Fila <filal41sng@gmail.com>
2021-06-18 15:13:36 +0300

    jumps between pages added

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,11 @@
-# def test_button(browser):
-#     try:
-#         assert  browser.find_elements_by_css_selector('button.btn:nth-child(3)'), 'Кнопка не найдена'
-#     finally:
-#         browser.quit()
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
